a9b08bda4052f87facd6ecc0419bbd041f23ce25
12jikan <jareem.e.hoff@gmail.com>
2019-01-25 15:53:16 -0500

    this push is actually pretty useful this time

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//~~~~~~~~~~ HTML Pages Start ~~~~~~~~~~//
+const homePage = fs.createReadStream('./static/index.html');
+const errPage = fs.createReadStream('./static/components/err/err404.html');
+
+//~~~~~~~~~~ HTML Pages End ~~~~~~~~~~//
+
+
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    
+    if(req.url === '/') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        homePage.pipe(res);
+    }
+    else {
+        res.writeHead(404, 'Content-type', 'text/html');
+        errPage.pipe(res);
+    }
 });
 
 server.listen(port, hostname, () => {
